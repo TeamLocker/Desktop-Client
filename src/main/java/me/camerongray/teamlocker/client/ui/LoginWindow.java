76621fb38ff6c24f19645de68679eb84b30197cc
Cameron Gray <cameron@camerongray.me>
2017-07-27 00:48:46 +0100

    Saving server, port and username on login screen now working

--- src/main/java/me/camerongray/teamlocker/client/ui/LoginWindow.java
+++ src/main/java/me/camerongray/teamlocker/client/ui/LoginWindow.java
@@ -6,6 +6,7 @@ import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
@@ -17,16 +18,22 @@ import me.camerongray.teamlocker.client.net.ApiClient;
 import me.camerongray.teamlocker.client.net.AuthenticationException;
 import me.camerongray.teamlocker.client.utils.UIHelpers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
+
 /**
  * Created by camerong on 01/06/17.
  */
-public class LoginWindow {
+public class LoginWindow implements Initializable {
     @FXML TextField txtServer;
     @FXML TextField txtPort;
     @FXML TextField txtUsername;
     @FXML PasswordField txtPassword;
     @FXML StackPane stackPaneRoot;
     @FXML AnchorPane anchorPaneControl;
+    @FXML CheckBox chkRememberServer;
+    @FXML CheckBox chkRememberUsername;
 
     @FXML
     void btnExit_Click(ActionEvent event) {
@@ -62,6 +69,21 @@ public class LoginWindow {
                 updateMessage("Getting user...");
                 User user = User.getSelfFromServer();
 
+                Preferences preferences = Preferences.userNodeForPackage(this.getClass());
+
+                preferences.remove("server");
+                preferences.remove("port");
+                preferences.remove("username");
+
+                if (chkRememberServer.isSelected()) {
+                    preferences.put("server", txtServer.getText());
+                    preferences.put("port", txtPort.getText());
+                }
+
+                if (chkRememberUsername.isSelected()) {
+                    preferences.put("username", txtUsername.getText());
+                }
+
                 return user;
             }
         };
@@ -73,9 +95,32 @@ public class LoginWindow {
                 MainWindow controller = new MainWindow(user);
                 UIHelpers.createStage(getClass().getResource("MainWindow.fxml"), controller, "TeamLocker", 640,
                         480, false).show();
+                // TODO: Hide this window!
             }
         });
 
         new Thread(task).start();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Preferences preferences = Preferences.userNodeForPackage(this.getClass());
+
+        String username = preferences.get("username", "");
+        String server = preferences.get("server", "");
+        String port = preferences.get("port", "");
+
+        if (!username.equals("")) {
+            txtUsername.setText(username);
+            chkRememberUsername.setSelected(true);
+        }
+
+        if (!server.equals("") && !port.equals("")) {
+            txtServer.setText(server);
+            txtPort.setText(port);
+            chkRememberServer.setSelected(true);
+        }
+
+        // TODO: Automatically put cursor in the correct text box?
     }
 }
